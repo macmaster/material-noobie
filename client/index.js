1eaf487a17354c38762a251ea229f8886b67adc3
Ronald Macmaster <ronnymacmaster@gmail.com>
2018-07-01 22:52:02 -0400

    clean up style. fix application errors. modify ignore

--- client/index.js
+++ client/index.js
@@ -8,14 +8,13 @@ import ReactDOM from "react-dom";
 
 // Application Content
 function Content(props) {
-  Component(props);
   return (
-    <p>{props.text}</p>
+    <h1>{props.title}</h1>
   );
 }
 
 var content = ReactDOM.render(
-  <Content text='Content goes here.' />,
+  <Content title='I love nature!' />,
   document.getElementById("content")
 );
 
